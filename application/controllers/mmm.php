ddf3ecb8addfc24dc68744ed2da9bf04af438e0a
Piyush Parkash <achyutapiyush@gmail.com>
2013-11-10 10:30:51 +0530

    Upto Now...

--- application/controllers/mmm.php
+++ application/controllers/mmm.php
@@ -2,6 +2,9 @@
 
 require_once '../id3/getid3/getid3.php';
 
+/**
+ * This the default controller of CI
+ */
 class mmm extends CI_Controller {
 
     public $id3;
@@ -9,16 +12,123 @@ class mmm extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->database();
+        $this->load->helper('url');
         $this->id3 = new getID3;
+        define("BASEURL", base_url());
+        define("INDEX", index_page());
     }
 
     public function index() {
-        $this->load->view("header");
-        if (file_exists("song.mp3")) {
-            //Process information that we have
-            $fileinfo = $this->id3->analyze("song.mp3");
+        $data['baseurl'] = BASEURL;
+        $data['index'] = INDEX;
+        $data['nodata'] = $this->db->query("select id from musicinfo")->num_rows();
+        $this->load->view("header", $data);
+        $this->load->view('main', $data);
+        $this->load->view("footer", $data);
+    }
+
+    public function submitdata() {
+        $data['baseurl'] = BASEURL;
+        $data['index'] = INDEX;
+        $data['error'] = FALSE;
+        $data['success'] = false;
+        $data['errmsg'] = "";
+        $allowed = array("audio/mp3");
+        if (isset($_POST['submit'])) {
+            //Check for file upload errors
+            if ($_FILES['musicfile']['error'] > 0) {
+                $data['error'] = true;
+                $data['errmsg'] = "File was not uploaded properly. Please try and upload again";
+            } elseif (!in_array($_FILES['musicfile']['type'], $allowed)) {
+                $data['error'] = true;
+                $data['errmsg'] = "File type not supported";
+            } else {
+                //Now we can upload the file
+                $fileinfo = $this->id3->analyze($_FILES['musicfile']["tmp_name"]);
+
+                //Add to database
+                if (!$this->addto_database($fileinfo['tags_html']['id3v2'])) {
+                    $data['error'] = TRUE;
+                    $data['errmsg'] = "Unable to add information in the database. Please Try again";
+                } else {
+                    $data['success'] = true;
+                }
+            }
         }
-        $this->load->view("footer");
+        $this->load->view("header", $data);
+        $this->load->view("submitdata", $data);
+        $this->load->view("footer", $data);
+    }
+
+    public function showall() {
+        $data['baseurl'] = BASEURL;
+        $data['index'] = INDEX;
+        $query = $this->db->query("select * from musicinfo");
+        $data['songs'] = $query->result();
+        $this->load->view('header', $data);
+        $this->load->view("showall", $data);
+        $this->load->view('footer', $data);
+    }
+
+    public function editinfo($id = NULL) {
+        if (is_null($id)) {
+            return FALSE;
+        }
+        $data['baseurl'] = BASEURL;
+        $data['index'] = INDEX;
+        $data['error'] = false;
+        if (isset($_POST['submit'])) {
+            $db_array = array(
+                "title" => $_POST['name'],
+                "artist" => $_POST['artist'],
+                "album" => $_POST['album']
+            );
+
+            $this->db->where('id', $id);
+            if (!$this->db->update('musicinfo', $db_array)) {
+                $data['error'] = true;
+            }
+        }
+
+        //Get the music details
+        $query = $this->db->query("select * from musicinfo where id=$id");
+        $data['song'] = $query->row(0);
+        $this->load->view('header', $data);
+        $this->load->view('editinfo', $data);
+        $this->load->view('footer', $data);
+    }
+
+    public function deleteinfo($id = NULL) {
+        if (is_null($id)) {
+            return FALSE;
+        }
+        $data['baseurl'] = BASEURL;
+        $data['index'] = INDEX;
+        $data['error'] = false;
+
+        if ($this->db->delete('musicinfo', array('id' => $id))) {
+            redirect('mmm/showall');
+        }
+    }
+
+    public function addto_database($data) {
+        if (!is_array($data)) {
+            return false;
+        }
+
+        //Prepare the data
+        $db_array = array("album" => $data['album'][0],
+            "artist" => $data['artist'][0],
+            "band" => $data['band'][0],
+            "composer" => $data['composer'][0],
+            "date" => $data['date'][0],
+            "genre" => $data['genre'][0],
+            "title" => $data['title'][0],
+            "year" => $data['year'][0],
+            "other" => json_encode($data)
+        );
+
+        return $this->db->insert("musicinfo", $db_array);
     }
 
 }
--- application/views/header.php
+++ application/views/header.php
@@ -1,12 +1,12 @@
-
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="style.css" />
-        <script src="jquery.js"></script>
-        <script src="bootstrap/dist/js/bootstrap.min.js"></script>
-        <script src="bootstrap/js/carousel.js"></script>
+        <link rel="stylesheet" href="<?php echo $baseurl ?>bootstrap/dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?php echo $baseurl ?>style.css" />
+        <script src="<?php echo $baseurl ?>jquery.js"></script>
+        <script src="<?php echo $baseurl ?>bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?php echo $baseurl ?>bootstrap/js/carousel.js"></script>
+        <script src="<?php echo $baseurl ?>jquery.validate.min.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-default" role="navigation">
@@ -18,13 +18,14 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">MMM</a>
+                <a class="navbar-brand" href="<?php echo "$baseurl/$index" ?>">MMM</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="#">Submit Data</a></li>
+                    <li><a href="<?php echo "$baseurl/$index" ?>/mmm/submitdata">Submit Data</a></li>
+                    <li><a href="<?php echo "$baseurl/$index" ?>/mmm/showall">Show All</a></li>
                 </ul>
                 <form class="navbar-form navbar-right" role="search">
                     <div class="form-group">
